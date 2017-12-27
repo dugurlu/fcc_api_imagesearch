bd2e42170782398b8541913b765a177bbd203da3
Glitch (spiritual-fisher) <none>
2017-12-27 20:56:00 +0000

    👂👌 Updated with Glitch

--- server.js
+++ server.js
@@ -1,44 +1,38 @@
-// User Story: I can get the image URLs, alt text and page urls for a set of images relating to a given search string.
-
-// User Story: I can paginate through the responses by adding a ?offset=2 parameter to the URL.
-
-// User Story: I can get a list of the most recently submitted search strings.
-
-// server.js
-// where your node app starts
-
 // init project
-var express = require('express');
-var app = express();
-var mongoose = require('mongoose')
-var assert = require('assert')
-var Query = require('./schema');
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+const assert = require('assert')
+const Query = require('./schema');
 
+// initialize search API
 const GoogleImages = require('google-images');
 const client = new GoogleImages(process.env.API_ID, process.env.API_KEY);
 
-var url = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+// initialize DB connection
+const url = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 mongoose.connect(url, {useMongoClient: true});
 mongoose.Promise = global.Promise;
-var db = mongoose.connection;
-
+const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get("/api/search/:query", (req, res) => {  
   // get image data
+  let page = req.query.offset ? req.query.offset : 1
   let images = []
   client
-    .search(req.params.query)
+    .search(req.params.query, {page: page})
     .then(data => {
-      data.forEach(i => {
-        // construct result object by selection only relevant object keys
-        let { url:imageUrl, description:altText, parentPage:pageUrl, thumbnail: { url:thumbnail } } = i
-        images.push({'imageUrl': imageUrl, 'altText': altText, 'pageUrl': pageUrl, 'thumbnail': thumbnail})
-      });
+      images = data.map( i => ({
+        imageUrl: i.url,
+        altText: i.description,
+        pageUrl: i.parentPage,
+        thumbnail: i.thumbnail.url
+      }))
     
       // save query info into db for /latest endpoint
-      var fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
-      var query = new Query({query: req.params.query, date: Date.now(), url: fullUrl});
+      const fullUrl = `${req.protocol}://${req.get('host')}${req.originalUrl}`;
+      const query = new Query({query: req.params.query, date: Date.now(), url: fullUrl});
       query.save((err) => {
         if(err) {
           console.log(err)
@@ -59,11 +53,16 @@ app.get("/api/latest", (req, res) => {
     .limit(10)
     .exec((err, queries) => {
       if(err) res.json([]);
-      res.json(queries)
+      let result = queries.map(query => ({
+        query: query.query,
+        url: query.url,
+        date: query.date
+      }))
+      res.json(result)
     })
 });
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+const listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
